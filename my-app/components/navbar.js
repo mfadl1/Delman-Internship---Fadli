496f9dbfe8ee126aa05628a17d2203b4359c7ac3
fadligun <m.fadli.gunardi@gmail.com>
2022-07-27 19:32:54 +0700

    implement breadcrumbs

--- my-app/components/navbar.js
+++ my-app/components/navbar.js
@@ -1,25 +1,41 @@
-import { Flex, Box, Divider, Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink, Stack,StackDivider, Heading, Text  } from "@chakra-ui/react"
+import { Flex, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Stack,StackDivider, Text  } from "@chakra-ui/react"
+import { useRouter } from 'next/router'
 
 export default function Navbar(){
+
+    const router = useRouter()
+
     return (
         <Flex align="center" py={3} px={4} position="absolute" width="100%">
             <Stack direction="row" divider={<StackDivider borderColor='gray.200' />} spacing={5} align="center">
                 <Text fontSize="2xl">delman.io</Text>
-                <Breadcrumb>
-                    <BreadcrumbItem isCurrentPage>
-                        <BreadcrumbLink href='/'>Internship Program</BreadcrumbLink>
-                    </BreadcrumbItem>
-
-                    <BreadcrumbItem>
-                        <BreadcrumbLink href='/change-title'>Title Tranformer</BreadcrumbLink>
-                    </BreadcrumbItem>
-
-                    <BreadcrumbItem >
-                        <BreadcrumbLink href='/fetching'>User Fetcher</BreadcrumbLink>
-                    </BreadcrumbItem>
-                </Breadcrumb>
+                {
+                    router.pathname == '/' ? 
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href='/'>Internship Program</BreadcrumbLink>
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                        
+                    : router.pathname == '/change-title' ?
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <BreadcrumbLink href='/'>Internship Program</BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem>
+                                <BreadcrumbLink href='/change-title'>Title Transformer</BreadcrumbLink>
+                            </BreadcrumbItem>
+                        </Breadcrumb>
+                    :
+                        <Breadcrumb>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href='/'>Internship Program</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href='/fetching'>User Fetcher</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        </Breadcrumb>
+                }
             </Stack>
         </Flex>
     );
